72bf48ede4c9783591f308be549cf8872f32be21
friku <riku.fujimoto@gmail.com>
2023-03-06 03:30:49 +0900

    会話文生成量調整

--- characterAI.py
+++ characterAI.py
@@ -17,6 +17,7 @@ import random
 from utils.voicevoxUtils import text2stream, makeWaveFile
 from utils.playWaveManager import WavQueuePlayer
 from utils.textEdit import remove_chars
+from utils.conversationRule import ConversationTimingChecker
 
 
 class OpenAILLM():
@@ -185,40 +186,44 @@ class ChatController():
 
 
 def main():
-    LLM = OpenAILLM()
-    ryuseiAI = CharacterAI(LLM, "龍星", contextDB_json, 13, 1.2)
-    metanAI = CharacterAI(LLM, "めたん", contextDB_json, 6, 1.2)
-    tumugiAI = CharacterAI(LLM, "つむぎ", contextDB_json, 8, 1.2)
-    zundamonAI = CharacterAI(LLM, "ずんだもん", contextDB_json, 7, 1.2)
-
-    characterAIs = [ryuseiAI, metanAI, tumugiAI, zundamonAI]
-
-    chatController = ChatController(characterAIs)
-    chatController.initContextAll()
-    chatController.addContextAll(
-        'system', "[プロデューサーとしての発言]\nあなたたちはラジオ出演者です。好きなゲームに関してトークしてください。適宜話題は変更してください。")
-
-    for i in range(100):
-        try:
-            chatController.getNextCharacterResponse()
-            # time.sleep(10)
-
-            if i % 8 == 0:
-                chatController.addContextAll(
-                    'system', "[プロデューサーとしての発言]\n別の話題を提案して、話してください。")
-            elif i % 4 == 0:
-                chatController.addContextAll(
-                    'system', "[プロデューサーとしての発言]\n話の深堀をしてください。")
-
-        except openai.error.RateLimitError:
-            print("rate limit error")
-            time.sleep(5)
-
-
-if __name__ == '__main__':
     try:
         audioPlayer = WavQueuePlayer('./tmpWaveDir')
         audioPlayer.play()
-        main()
+        
+        LLM = OpenAILLM()
+        ryuseiAI = CharacterAI(LLM, "龍星", contextDB_json, 13, 1.2)
+        metanAI = CharacterAI(LLM, "めたん", contextDB_json, 6, 1.2)
+        tumugiAI = CharacterAI(LLM, "つむぎ", contextDB_json, 8, 1.2)
+        zundamonAI = CharacterAI(LLM, "ずんだもん", contextDB_json, 7, 1.2)
+
+        characterAIs = [ryuseiAI, metanAI, tumugiAI, zundamonAI]
+
+        conversationTimingChecker = ConversationTimingChecker()
+        chatController = ChatController(characterAIs)
+        chatController.initContextAll()
+        chatController.addContextAll(
+            'system', "[プロデューサーとしての発言]\nあなたたちはラジオ出演者です。好きなゲームに関してトークしてください。適宜話題は変更してください。")
+
+        for i in range(100):
+            try:
+                conversationTimingChecker.check_conversation_timing_with_delay(audioPlayer.get_file_queue_length)
+                chatController.getNextCharacterResponse()
+                # time.sleep(10)
+
+                if i % 8 == 0:
+                    chatController.addContextAll(
+                        'system', "[プロデューサーとしての発言]\n別の話題を提案して、話してください。")
+                elif i % 4 == 0:
+                    chatController.addContextAll(
+                        'system', "[プロデューサーとしての発言]\n話の深堀をしてください。")
+
+            except openai.error.RateLimitError:
+                print("rate limit error")
+                time.sleep(5)
+                
     except KeyboardInterrupt:
         audioPlayer.stop()
+
+if __name__ == '__main__':
+    main()
+
