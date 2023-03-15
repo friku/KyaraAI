292df80794fe778a90b675dd23c4c555a5215b3d
friku <riku.fujimoto@gmail.com>
2023-03-15 20:18:41 +0900

    add PromptMaker.py

---
+++ utils/PromptMaker.py
@@ -0,0 +1,54 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from utils.contextDB import contextDB_json
+from pathlib import Path
+import pdb
+
+
+class PromptMaker(object):
+    def __init__(self, contextDB, identityPath, characterName):
+        self.characterName = characterName
+        self.contextDB = contextDB
+        self.identityPath = identityPath
+        self.identity = self.getIdentity(self.identityPath)
+
+    def getIdentity(self, identityPath): # identityの取得
+        with open(identityPath, 'r', encoding="utf-8") as f:
+            identityContext = f.read()
+        return {"role": "user", "content": identityContext}
+
+    def getOutputFormat(self,): # 出力フォーマットの取得
+        context = f"""
+あなたは{self.characterName}として下記のxxxを埋める出力を必ず毎回してください。
+発言は１文だけにしてください。
+
+-------
+
+[{self.characterName}としての発言]
+xxx
+"""
+        outputFormat = {"role": "system", "content": context}
+        return outputFormat
+    
+    def getPrompt(self, contextNum: int = 8):
+        prompt = []
+        context = self.contextDB.get()[-contextNum:]
+        outputFormat = self.getOutputFormat()
+        
+        prompt.append(self.identity)
+        prompt.append(outputFormat)
+        prompt += context
+        return prompt
+        
+
+
+def main():
+    contextDB = contextDB_json(Path("characterConfig/ネオン/context.json"))
+    promptMaker = PromptMaker(
+        contextDB, "characterConfig/ネオン/identity.txt", "ネオン")
+    print(promptMaker.getPrompt())
+
+
+if __name__ == '__main__':
+    main()
